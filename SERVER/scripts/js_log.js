3bf12c3727bfa6b4fbae7c198ec35e906e533391
ivanwfr <ivanw@orange.fr>
2026-09-24 01:14:32 +0200

    Devtools console fg bg styling

--- SERVER/scripts/js_log.js
+++ SERVER/scripts/js_log.js
@@ -1,5 +1,5 @@
 //┌────────────────────────────────────────────────────────────────────────────┐
-//│ js_log.js        ● $APROJECTS/LANServer/SERVER      ● _TAG (260922:23h:43) │
+//│ js_log.js        ● $APROJECTS/LANServer/SERVER      ● _TAG (260923:21h:52) │
 //└────────────────────────────────────────────────────────────────────────────┘
 /* jshint {{{*/
 
@@ -40,6 +40,19 @@ let tag_this = false || log_this;
     const lbX  = [ lb0 ,lb1 ,lb2 ,lb3 ,lb4 ,lb5 ,lb6 ,lb7 ,lb8 ,lb9 ];
     const lbB  = lb0 +"font-size: 150%; border-radius: 1em; padding: 0 1em; border: 1px solid red;";
 
+    const lf1  = "color:#964B00;";
+    const lf2  = "color:#FF0000;";
+    const lf3  = "color:#FFA500;";
+    const lf4  = "color:#FFFF00;";
+    const lf5  = "color:#9ACD32;";
+    const lf6  = "color:#6495ED;";
+    const lf7  = "color:#EE82EE;";
+    const lf8  = "color:#A0A0A0;";
+    const lf9  = "color:#FFFFFF;";
+    const lf0  = "color:#707070; text-shadow:#000 2px 2px 1px;"; /* offset-x offset-y blur-radius */
+    const lfX  = [ lf0 ,lf1 ,lf2 ,lf3 ,lf4 ,lf5 ,lf6 ,lf7 ,lf8 ,lf9 ];
+
+
     /* eslint-enable  no-unused-vars */
     //}}}
     /*_ console_clear {{{*/
@@ -68,10 +81,10 @@ let tag_this = false || log_this;
     //│ UTIL
     //└────────────────────────────────────────────────────────────────────────┘
     /* ● ellipsis {{{*/
-    let ellipsis = function(str, n)
+    let ellipsis = function(str, n=128)
     {
         return  str
-            ? ((str.length > n) ? str.slice(0, n - 1) + "…" : str)
+            ? ((str.length > n) ? str.slice(0, n - 1) + "… " : str)
             : ""
         ;
     };
@@ -88,18 +101,21 @@ let tag_this = false || log_this;
 
     // return {{{
     return { name: "js_log"
+
+        ,    console_clear
         ,    log
         ,    toggle
         ,    is_logging
         ,    is_tagging
+
         ,    get_src_link
-        ,    console_clear
+        ,    ellipsis
 
         ,    lbB
         ,    lbX
+        ,    lfX
         , ...lbX // js_log.lb0 .. js_log.lb9
-
-        ,    ellipsis
+        , ...lfX // js_log.lf0 .. js_log.lf9
     };
     //}}}
 }());
